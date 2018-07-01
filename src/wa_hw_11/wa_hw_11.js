4121a1995993eeb030c1e6e5648593254236957d
Oleh Moroz <olehmoroz8@gmail.com>
2018-07-01 14:09:19 +0300

    hw_12

--- src/wa_hw_11/wa_hw_11.js
+++ src/wa_hw_11/wa_hw_11.js
@@ -1,23 +1,19 @@
 import './wa_hw_11.scss'
 
-const accordion = document.getElementsByClassName("accordion_title");
+const accordion = document.querySelectorAll(".accordion_title");
 
+for(let i = 0; i < accordion.length; i++){
 
-
-for(let i = 0; i < itemsList.length; i++){
-
-  fullAccordion(element, itemsList[i]);
-
-  accordion[i].addEventListener('click', function() {
-  let accordionBody = this.nextElementSibling;
-  let activeAccordion = document.querySelector('.active');
-  if (activeAccordion === null) {
-      accordionBody.classList.add('active');
-      this.classList.add("triagles");
-  } 
-  else {
-      accordionBody.classList.remove('active');
-      this.classList.remove("triagles");
+accordion[i].addEventListener('click', function() {
+let accordionBody = this.nextElementSibling;
+let activeAccordion = document.querySelector('.active');
+if (activeAccordion === null) {
+    accordionBody.classList.add('active');
+    this.classList.add("triagles");
+} 
+else {
+    accordionBody.classList.remove('active');
+    this.classList.remove("triagles");
   } 
 });
 }
--- src/wa_hw_12/wa_hw_12.js
+++ src/wa_hw_12/wa_hw_12.js
@@ -2,7 +2,7 @@ import './wa_hw_12.scss'
 import { accordionFull } from './script/script';
 
 const element = document.querySelector('.accordion_element');
-const accordion = document.getElementsByClassName("accordion_title");
+const accordion = document.querySelectorAll(".accordion_title");
 
 const itemsList = [
     {
@@ -27,21 +27,24 @@ const itemsList = [
 for(let i = 0; i < itemsList.length; i++){
 
     accordionFull(element, itemsList[i]);
-
-    accordion[i].addEventListener('click', function() {
+}
+    
+for(let i = 0; i < accordion.length; i++){
+    
+accordion[i].addEventListener('click', function() {
     let accordionBody = this.nextElementSibling;
     let activeAccordion = document.querySelector('.active');
-    if (accordionBody === activeAccordion) {
+    if (activeAccordion === accordionBody) {
         accordionBody.classList.remove('active');
         this.classList.remove("triagles");
     } 
-    else if(accordionBody !== activeAccordion){
+    else if(activeAccordion === null){
         accordionBody.classList.add('active');
         this.classList.add("triagles");
-    } 
+    }
     else {
         accordionBody.classList.remove('active');
         this.classList.remove("triagles");
-    }
-  });
+    } 
+});
 }
